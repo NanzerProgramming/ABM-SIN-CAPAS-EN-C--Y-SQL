4f1fd1ab72474206af5b3e7b05e548310108ee5e
Nanzer_Programming <77675614+NanzerProgramming@users.noreply.github.com>
2022-05-11 15:49:37 -0300

    Add files via upload

---
+++ ABM_con_el_PROFE/Empleados.Designer.cs
@@ -0,0 +1,374 @@
+﻿
+namespace ABM_con_el_PROFE
+{
+    partial class Empleados
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvEmpleados = new System.Windows.Forms.DataGridView();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPiso = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCalle = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.Ciudad = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDepto = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCiudad = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dtpFechaNac = new System.Windows.Forms.DateTimePicker();
+            this.btnRefrescar = new System.Windows.Forms.Button();
+            this.txtLegajo = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvEmpleados
+            // 
+            this.dgvEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmpleados.Location = new System.Drawing.Point(18, 187);
+            this.dgvEmpleados.Name = "dgvEmpleados";
+            this.dgvEmpleados.Size = new System.Drawing.Size(628, 214);
+            this.dgvEmpleados.TabIndex = 0;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(558, 126);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(85, 23);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Location = new System.Drawing.Point(558, 10);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(85, 23);
+            this.btnGrabar.TabIndex = 3;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(558, 39);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(85, 23);
+            this.btnBorrar.TabIndex = 4;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(558, 68);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(85, 23);
+            this.btnModificar.TabIndex = 5;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(208, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Piso";
+            // 
+            // txtPiso
+            // 
+            this.txtPiso.Location = new System.Drawing.Point(259, 104);
+            this.txtPiso.Name = "txtPiso";
+            this.txtPiso.Size = new System.Drawing.Size(100, 20);
+            this.txtPiso.TabIndex = 7;
+            this.txtPiso.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPiso_KeyDown);
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(78, 49);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 9;
+            this.txtApellido.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtApellido_KeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Apellido";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(78, 81);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 11;
+            this.txtNombre.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtNombre_KeyDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 26);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "    Fecha \r\nNacimiento\r\n";
+            // 
+            // txtCalle
+            // 
+            this.txtCalle.Location = new System.Drawing.Point(259, 49);
+            this.txtCalle.Name = "txtCalle";
+            this.txtCalle.Size = new System.Drawing.Size(100, 20);
+            this.txtCalle.TabIndex = 15;
+            this.txtCalle.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtCalle_KeyDown);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(208, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Calle";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(259, 77);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 17;
+            this.txtNumero.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtNumero_KeyDown);
+            // 
+            // Ciudad
+            // 
+            this.Ciudad.AutoSize = true;
+            this.Ciudad.Location = new System.Drawing.Point(208, 80);
+            this.Ciudad.Name = "Ciudad";
+            this.Ciudad.Size = new System.Drawing.Size(44, 13);
+            this.Ciudad.TabIndex = 16;
+            this.Ciudad.Text = "Número";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(208, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "DOMICILIO";
+            // 
+            // txtDepto
+            // 
+            this.txtDepto.Location = new System.Drawing.Point(427, 46);
+            this.txtDepto.Name = "txtDepto";
+            this.txtDepto.Size = new System.Drawing.Size(100, 20);
+            this.txtDepto.TabIndex = 20;
+            this.txtDepto.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtDepto_KeyDown);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(376, 49);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Depto";
+            // 
+            // txtCiudad
+            // 
+            this.txtCiudad.Location = new System.Drawing.Point(427, 76);
+            this.txtCiudad.Name = "txtCiudad";
+            this.txtCiudad.Size = new System.Drawing.Size(100, 20);
+            this.txtCiudad.TabIndex = 22;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(376, 79);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Ciudad";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(12, 21);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(135, 13);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "DATOS PERSONALES";
+            // 
+            // dtpFechaNac
+            // 
+            this.dtpFechaNac.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaNac.Location = new System.Drawing.Point(78, 114);
+            this.dtpFechaNac.Name = "dtpFechaNac";
+            this.dtpFechaNac.Size = new System.Drawing.Size(101, 20);
+            this.dtpFechaNac.TabIndex = 24;
+            this.dtpFechaNac.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dtpFechaNac_KeyDown);
+            // 
+            // btnRefrescar
+            // 
+            this.btnRefrescar.Location = new System.Drawing.Point(558, 97);
+            this.btnRefrescar.Name = "btnRefrescar";
+            this.btnRefrescar.Size = new System.Drawing.Size(85, 23);
+            this.btnRefrescar.TabIndex = 25;
+            this.btnRefrescar.Text = "Refresh";
+            this.btnRefrescar.UseVisualStyleBackColor = true;
+            this.btnRefrescar.Click += new System.EventHandler(this.btnRefrescar_Click_1);
+            // 
+            // txtLegajo
+            // 
+            this.txtLegajo.Location = new System.Drawing.Point(428, 155);
+            this.txtLegajo.Name = "txtLegajo";
+            this.txtLegajo.Size = new System.Drawing.Size(70, 20);
+            this.txtLegajo.TabIndex = 27;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(377, 158);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(39, 13);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "Legajo";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(376, 110);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(121, 39);
+            this.label11.TabIndex = 28;
+            this.label11.Text = "IDENTIFICADOR \r\nPARA BORRAR O \r\nMODIFICAR DATOS";
+            // 
+            // Empleados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 412);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtLegajo);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.btnRefrescar);
+            this.Controls.Add(this.dtpFechaNac);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtCiudad);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtDepto);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.Ciudad);
+            this.Controls.Add(this.txtCalle);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPiso);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.dgvEmpleados);
+            this.Name = "Empleados";
+            this.Text = "Empleados";
+            this.Shown += new System.EventHandler(this.Empleados_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvEmpleados;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPiso;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCalle;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label Ciudad;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDepto;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCiudad;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DateTimePicker dtpFechaNac;
+        private System.Windows.Forms.Button btnRefrescar;
+        private System.Windows.Forms.TextBox txtLegajo;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ ABM_con_el_PROFE/Empleados.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ABM_con_el_PROFE
+{
+    public partial class Empleados : Form
+    {
+        public Empleados()
+        {
+            InitializeComponent();
+        }
+
+        #region CONECCION
+        SqlConnection conn = new SqlConnection("Password=7d26t13ar4;Persist Security Info=True;User ID=Nanzer_SQLLogin_1;Initial Catalog=ProyectoNanzerClima;Data Source=ProyectoNanzerClima.mssql.somee.com"); //CONEXION PÚBLICA
+        #endregion
+
+        #region SELECT
+        private void btnRefrescar_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                SqlCommand consulta = new SqlCommand("select * from Empleados", conn);
+                SqlDataAdapter adaptador = new SqlDataAdapter();
+                adaptador.SelectCommand = consulta;
+                DataTable tabla = new DataTable();
+                adaptador.Fill(tabla);
+                dgvEmpleados.DataSource = tabla;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+                Clear();
+            }
+        }
+        #endregion
+
+        #region UPDATE
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = @"UPDATE Empleados SET Apellido = @Apellido, Nombre = @Nombre, 
+                                FechaNacimiento =@FechaNacimiento, CalleDom = @CalleDom, 
+                                NumeroDom = @NumeroDom, PisoDom = @PisoDom, DeptoDom = @DeptoDom, 
+                                CiudadDom = @CiudadDom WHERE Legajo = @Legajo";
+                conn.Open();
+                SqlCommand comando = new SqlCommand(query, conn);
+
+                comando.Parameters.AddWithValue("@Apellido", txtApellido.Text);
+                comando.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                comando.Parameters.AddWithValue("@FechaNacimiento", dtpFechaNac.Value);
+                comando.Parameters.AddWithValue("@CalleDom", txtCalle.Text);
+                comando.Parameters.AddWithValue("@NumeroDom", txtNumero.Text);
+                comando.Parameters.AddWithValue("@PisoDom", txtPiso.Text);
+                comando.Parameters.AddWithValue("@DeptoDom", txtDepto.Text);
+                comando.Parameters.AddWithValue("@CiudadDom", txtCiudad.Text);
+                comando.Parameters.AddWithValue("@Legajo", txtLegajo.Text);
+
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+                MessageBox.Show("Actualizado, por favor refresque la tabla.");
+            }
+        }
+        #endregion
+
+        #region DELETE
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                string query = @"DELETE from Empleados WHERE Legajo = @Legajo";
+                SqlCommand comando = new SqlCommand(query, conn);
+                comando.Parameters.AddWithValue("@Legajo", txtLegajo.Text);
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+                MessageBox.Show("Dato eliminado con exito, por favor refresque la tabla.");
+            }
+        }
+        #endregion
+
+        #region INSERT
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+
+                SqlCommand query = new SqlCommand("INSERT INTO Empleados(Apellido, Nombre, FechaNacimiento, CalleDom, NumeroDom, PisoDom, DeptoDom, CiudadDom) values (@Apellido, @Nombre, @FechaNacimiento, @CalleDom, @NumeroDom, @PisoDom, @DeptoDom, @CiudadDom)", conn);
+
+                query.Parameters.AddWithValue("@Apellido", txtApellido.Text);
+                query.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                query.Parameters.AddWithValue("@FechaNacimiento", dtpFechaNac.Value);
+                query.Parameters.AddWithValue("@CalleDom", txtCalle.Text);
+                query.Parameters.AddWithValue("@NumeroDom", txtNumero.Text);
+                query.Parameters.AddWithValue("@PisoDom", txtPiso.Text);
+                query.Parameters.AddWithValue("@DeptoDom", txtDepto.Text);
+                query.Parameters.AddWithValue("@CiudadDom", txtCiudad.Text);
+
+                query.ExecuteNonQuery();
+
+                //Recarga la lista!
+
+                SqlCommand consulta = new SqlCommand("select * from Empleados", conn);
+                SqlDataAdapter adaptador = new SqlDataAdapter();
+                adaptador.SelectCommand = consulta;
+                DataTable tabla = new DataTable();
+                adaptador.Fill(tabla);
+                dgvEmpleados.DataSource = tabla;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+                Clear();
+            }
+
+        }
+
+
+        #endregion
+
+        #region EVENTOS
+        private void txtApellido_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtNombre.Focus();
+            }
+        }
+
+        private void txtNombre_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                dtpFechaNac.Focus();
+            }
+        }
+
+        private void dtpFechaNac_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtCalle.Focus();
+            }
+        }
+
+        private void txtCalle_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtNumero.Focus();
+            }
+        }
+
+        private void txtNumero_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtPiso.Focus();
+            }
+        }
+
+        private void txtPiso_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtDepto.Focus();
+            }
+        }
+
+        private void txtDepto_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtCiudad.Focus();
+            }
+        }
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Empleados_Shown(object sender, EventArgs e)
+        {
+            txtApellido.Focus();
+        }
+        #endregion
+
+        #region FUNCIONES
+        private void Clear()
+        {
+            txtApellido.Text = string.Empty;
+            txtNombre.Text = string.Empty;
+            dtpFechaNac.Value = DateTime.Now;
+            txtCalle.Text = string.Empty;
+            txtNumero.Text = string.Empty;
+            txtPiso.Text = string.Empty;
+            txtDepto.Text = string.Empty;
+            txtCiudad.Text = string.Empty;
+        }
+
+        #endregion
+
+    }
+}
